966c180098fc3fee0f4a384c46f7efd822e28cd7
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-09-06 23:31:30 -0500

    Bug de compilación de tailwind solucionado

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-
+require("laravel-mix-tailwind");
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -12,4 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .tailwind();
