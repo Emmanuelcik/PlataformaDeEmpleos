b7334c5eea73fd6b3ab27fdd1d29152f1e829649
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-09-08 21:01:41 -0500

    Vistas de vacantes

---
+++ resources/views/UI/adminnav.blade.php
@@ -0,0 +1,2 @@
+<a href="#" class="text-white text-sm uppercase font-bold p-3">Ver Vacantes</a>
+<a href="#" class="text-white text-sm uppercase font-bold p-3">Nueva Vacante</a>
---
+++ resources/views/vacantes/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('navegacion')
+    @include("UI.adminnav")
+@endsection
+@section('content')
+    <h1 class="text-2xl text-center mt-10">Administra tus vacantes</h1>
+
+@endsection
