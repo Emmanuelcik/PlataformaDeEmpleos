06df39053bc5cfee28efb2910a3a4370d525ab33
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-09-08 20:20:51 -0500

    La parte de restaurar contraseña esta completa

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,42 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
+<div class="container contenedor mx-auto">
+    <div class="flex flex-wrap justify-center">
+        <div class="w-full max-w-sm">
+            <div class="flex flex-col break-words bg-white border-2 shadow-ms mt-20">
+                <div class="bg-gray-300 text-gray-700 uppercase text-center py-3 px-6 mb-0">
+                    {{__("Reset Password")}}
+                </div>
+                @if (session("status"))
+                    <span class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 w-full mt-5 mxy-3 block text-sm">{{session("status")}}</span>
+                @endif
+                    <form novalidate class="py-10 px-5" method="POST" action="{{ route('password.email') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        <div class="flex flex-wrap mb-6">
+                            <label for="email" class="block text-gray-700 text-sm mb-2">{{ __('E-Mail Address') }}</label>
+                                <input id="email" type="email" class="p-3 bg-gray-200 rounded form-input w-full @error('email') border-red-500 border @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 w-full mt-5 text-sm" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                        <div class="flex flex-wrap ">
+                                <button type="submit" class="bg-teal-500 w-full hover:bg-teal-700 text-gray-100 p-3 focus:online-none focus:shadow-outline uppercase font-bold">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
-                            </div>
                         </div>
                     </form>
                 </div>
