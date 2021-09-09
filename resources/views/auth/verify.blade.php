147555404cab58a52cb08340b540e28e669e575b
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-09-08 19:56:44 -0500

    Modificada la vista de confirmación de correo con tailwind

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,28 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+<div class="container mx-auto mt-20">
+                <div class="text-2xl my-5 text-center">{{ __('Verify Your Email Address') }}</div>
 
-                <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
                             {{ __('A fresh verification link has been sent to your email address.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                    <p class="text-center">{{ __('Before proceeding, please check your email for a verification link.') }}</p>
+                    <p class="text-center my-5 ">{{ __('If you did not receive the email') }}, </p>
+                    <form class="contenedor d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="contenedor text-center mt-10 max-w-sm bg-teal-500 w-full hover:bg-teal-700 text-gray-100 p-3 focus:online-none focus:shadow-outline uppercase font-bold">{{ __('click here to request another') }}</button>
                     </form>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
+{{-- </div> --}}
 @endsection
