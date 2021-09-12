51e7c4f3853385181cd32484b2b229f7cccc7f32
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-09-11 23:04:43 -0500

    Vista para crear vacantes

---
+++ resources/views/vacantes/create.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('navegacion')
+    @include('UI.Adminnav')
+@endsection
+
+@section('content')
+    <h1 class="text-2xl text-center mt-10">Nueva vacante</h1>
+
+@endsection
