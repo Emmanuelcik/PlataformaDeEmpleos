d8b5cf3134e48aa8798ab442f1aa8538be4f19e6
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-09-25 23:53:33 -0500

    En el show ya se muestra la fecha con formato diffForHumans y los primeros campos

--- resources/views/vacantes/show.blade.php
+++ resources/views/vacantes/show.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<h1 class="text-3xl text-center mt-10">{{$vacante->titulo}}</h1>
+    <h1 class="text-3xl text-center mt-10">{{$vacante->titulo}}</h1>
+    <div class="mt-10 mb-20 md:flex items-start">
+        <div class="md:w-3/5">
+            <p class="block text-gray-700 font-bold my-2">
+                Publicado: <span class="font-normal"> {{$vacante->created_at->diffForHumans()}}</span>
+            </p>
+            <p class="block text-gray-700 font-bold my-2">
+                Categoria: <span class="font-normal"> {{$vacante->categoria->nombre}}</span>
+            </p>
+            <p class="block text-gray-700 font-bold my-2">
+                Salario: <span class="font-normal"> {{$vacante->salario_id}}</span>
+            </p>
+        </div>
+        <div class="md:w-2/5">
+            2
+        </div>
+    </div>
 @endsection
