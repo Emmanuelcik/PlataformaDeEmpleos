5b2b91b214d6e52ad344037c72fb35a91a5f0077
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-10-04 23:06:22 -0500

    Estilos y maquetación de todas las vacantes disponibles en el home de la página

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -31,8 +31,28 @@
 
         <ul class="mt-10 grid grid-cols-1 md:grid-cols-2 gap-8">
             @foreach ($vacantes as $vacante)
-                <li>
-                    {{$vacante->titulo}}
+                <li class="p-10 border border-gray-300 bg-white shado">
+                    <h2 class="text-2xl font-bold text-teal-500">{{$vacante->titulo}}</h2>
+
+                    <p class="block text-gray-700 font-normal my-2">
+                        {{$vacante->categoria->nombre}}
+                    </p>
+
+                    <p class="block text-gray-700 font-normal my-2">
+                        Ubicación:
+                        <span class="font-bold"> {{$vacante->ubicacion->nombre}} </span>
+                    </p>
+
+                    <p class="block text-gray-700 font-normal my-2">
+                        Experiencia:
+                        <span class="font-bold"> {{$vacante->experiencia->nombre}} </span>
+                    </p>
+
+                    <a
+                        class="bg-teal-500 text-gray-100 mt-2 px-4 py-2 inline-block rounded font-bold text-sm"
+                        href="{{route("vacantes.show", ["vacante" =>$vacante->id])}}"
+                    >
+                    Ver Vacante</a>
                 </li>
             @endforeach
         </ul>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,9 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 
     @yield('styles')
     <!-- Styles -->
